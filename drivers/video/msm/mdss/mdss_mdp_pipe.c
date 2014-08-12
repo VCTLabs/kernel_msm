15436efa8086b6fa4a96c2081c0499cabd14ccd9
Justin Philip <jphili@codeaurora.org>
2014-08-12 13:46:54 +0530

    mdss: Add MDP_SMP_FORCE_ALLOC mdp flag
    MDP_SMP_FORCE_ALLOC flag is used to allow SMP
    allocations even when there is mismatch between
    allocated and requested SMPs for a pipe.
    User space can make use of this flag in extreme scenarios
    where SMP allocations need to pass like GPU composition
    and playback of protected  or secure videos.
    
    Change-Id: I369b4361e7e2bbfc8150add467678c4ef8d5cfb6
    Signed-off-by: Justin Philip <jphili@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -365,13 +365,8 @@ int mdss_mdp_smp_reserve(struct mdss_mdp_pipe *pipe)
 		pipe->mixer_left->type == MDSS_MDP_MIXER_TYPE_WRITEBACK)
 		wb_mixer = 1;
 
-	/*
-	 * Don't want to allow SMP changes for backend composition pipes
-	 * inorder to preserve SMPs as much as possible.
-	 * On the contrary for non backend composition pipes we should
-	 * allow SMP allocations to prevent composition failures.
-	 */
-	force_alloc = !(pipe->flags & MDP_BACKEND_COMPOSITION);
+	force_alloc = pipe->flags & MDP_SMP_FORCE_ALLOC;
+
 	mutex_lock(&mdss_mdp_smp_lock);
 	for (i = (MAX_PLANES - 1); i >= ps.num_planes; i--) {
 		if (bitmap_weight(pipe->smp_map[i].allocated, SMP_MB_CNT)) {
