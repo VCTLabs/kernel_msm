01365935ca3ebc8b80a5914d8d0ac8b634ccca51
Maxime Bergeron <mazout360@gmail.com>
2014-10-16 21:21:59 -0400

    Base porting to linux 3 .10 (ported changed files, except include changes)

--- arch/arm/mach-msm/include/mach/timex.h
+++ arch/arm/mach-msm/include/mach/timex.h
@@ -1,6 +1,6 @@
 /* arch/arm/mach-msm/include/mach/timex.h
  *
- * Copyright (C) 2007 Google, Inc.
+ * Copyright (C) 2012,2013 LGE, Inc
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -12,12 +12,53 @@
  * GNU General Public License for more details.
  *
  */
+#ifndef __ARCH_MSM_BOARD_LGE_H
+#define __ARCH_MSM_BOARD_LGE_H
+
+enum {
+	HW_REV_EVB1 = 0,
+	HW_REV_EVB2,
+	HW_REV_A,
+	HW_REV_B,
+	HW_REV_C,
+	HW_REV_D,
+	HW_REV_E,
+	HW_REV_F,
+	HW_REV_G,
+	HW_REV_H,
+	HW_REV_1_0,
+	HW_REV_1_1,
+	HW_REV_1_2,
+	HW_REV_MAX
+};
 
 #ifndef __ASM_ARCH_MSM_TIMEX_H
 #define __ASM_ARCH_MSM_TIMEX_H
 
-#define CLOCK_TICK_RATE		1000000
+enum lge_boot_mode_type {
+	LGE_BOOT_MODE_NORMAL = 0,
+	LGE_BOOT_MODE_CHARGER,
+	LGE_BOOT_MODE_CHARGERLOGO,
+	LGE_BOOT_MODE_FACTORY,
+	LGE_BOOT_MODE_FACTORY2,
+	LGE_BOOT_MODE_PIFBOOT
+};
 
+enum lge_boot_mode_type lge_get_boot_mode(void);
+
+#define UART_MODE_ALWAYS_OFF_BMSK   BIT(0)
+#define UART_MODE_ALWAYS_ON_BMSK    BIT(1)
+#define UART_MODE_INIT_BMSK         BIT(2)
+#define UART_MODE_EN_BMSK           BIT(3)
+
+extern unsigned int lge_get_uart_mode(void);
+extern void lge_set_uart_mode(unsigned int um);
+
+#ifdef CONFIG_PSTORE_RAM
+#define LGE_RAM_CONSOLE_SIZE (128 * SZ_1K * 2)
+#define LGE_PERSISTENT_RAM_SIZE (SZ_1M)
+
+#define CLOCK_TICK_RATE         1000000
 #ifdef CONFIG_HAVE_ARCH_HAS_CURRENT_TIMER
 #define ARCH_HAS_READ_CURRENT_TIMER
 #endif
