2ee852004b6e7815dfba0b2e07e826ed778b2651
Stephen Arnold <stephen.arnold42@gmail.com>
2017-04-28 18:05:57 -0700

    msm_isp_axi_util.c: old version; incomplete fixes (WIP)
    Signed-off-by: Stephen Arnold <stephen.arnold42@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -457,18 +457,12 @@ void msm_isp_sof_notify(struct vfe_device *vfe_dev,
 	enum msm_vfe_input_src frame_src, struct msm_isp_timestamp *ts) {
 	struct msm_isp_event_data sof_event;
 
-	vfe_dev->skip_ping_pong_cfg = 0;
-	if (vfe_dev->skip_isp_send_event) {
-		vfe_dev->skip_ping_pong_cfg = 1;
-		return;
-	}
-
 	switch (frame_src) {
 	case VFE_PIX_0:
 		vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id++;
 		if (vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id == 0)
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id = 1;
-		ISP_DBG("%s: PIX0 frame id: %lu\n", __func__,
+		ISP_DBG("%s: PIX0 frame id: %u\n", __func__,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id);
 		break;
 	case VFE_RAW_0:
