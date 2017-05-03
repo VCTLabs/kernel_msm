fe62c4089ab0d8c314190273607f6a47e17c9272
Stephen Arnold <stephen.arnold42@gmail.com>
2017-05-03 12:38:54 -0700

    board-8974-gpiomux.c: fp2 updates; note sd_card did not exist in this version of the file
    Signed-off-by: Stephen Arnold <stephen.arnold42@gmail.com>

--- arch/arm/mach-msm/board-8974-gpiomux.c
+++ arch/arm/mach-msm/board-8974-gpiomux.c
@@ -280,14 +280,14 @@ static struct gpiomux_setting atmel_resout_act_cfg = {
 
 static struct gpiomux_setting atmel_int_act_cfg = {
 	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_8MA,
+	.drv = GPIOMUX_DRV_16MA,
 	.pull = GPIOMUX_PULL_UP,
 };
 
 static struct gpiomux_setting atmel_int_sus_cfg = {
 	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_DOWN,
+	.drv = GPIOMUX_DRV_16MA,
+	.pull = GPIOMUX_PULL_UP,
 };
 
 static struct gpiomux_setting atmel_i2cmode_act_cfg = {
@@ -347,14 +347,14 @@ static struct msm_gpiomux_config msm_touch_i2cmode_configs[] __initdata = {
 
 static struct msm_gpiomux_config msm_touch_configs[] __initdata = {
 	{
-		.gpio      = 60,		/* TOUCH RESET */
+		.gpio      = 55,		/* TOUCH RESET */
 		.settings = {
 			[GPIOMUX_ACTIVE] = &atmel_resout_act_cfg,
 			[GPIOMUX_SUSPENDED] = &atmel_resout_sus_cfg,
 		},
 	},
 	{
-		.gpio      = 61,		/* TOUCH IRQ */
+		.gpio      = 28,		/* TOUCH IRQ */
 		.settings = {
 			[GPIOMUX_ACTIVE] = &atmel_int_act_cfg,
 			[GPIOMUX_SUSPENDED] = &atmel_int_sus_cfg,
@@ -571,7 +571,7 @@ static struct msm_gpiomux_config msm_rumi_blsp_configs[] __initdata = {
 
 static struct msm_gpiomux_config msm_lcd_configs[] __initdata = {
 	{
-		.gpio = 58,
+		.gpio = 25,
 		.settings = {
 			[GPIOMUX_ACTIVE]    = &lcd_en_act_cfg,
 			[GPIOMUX_SUSPENDED] = &lcd_en_sus_cfg,
@@ -707,13 +707,13 @@ static struct msm_gpiomux_config msm_blsp_configs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[0],
 		},
 	},
-	{
-		.gpio      = 55,		/* BLSP2 QUP4 SPI_CS0_N */
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_spi_config,
-			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[0],
-		},
-	},
+	//{
+	//	.gpio      = 55,		/* BLSP2 QUP4 SPI_CS0_N */
+	//	.settings = {
+	//		[GPIOMUX_ACTIVE] = &gpio_spi_config,
+	//		[GPIOMUX_SUSPENDED] = &gpio_suspend_config[0],
+	//	},
+	//},
 	{	/* NFC */
 		.gpio      = 29,		/* BLSP1 QUP6 I2C_DAT */
 		.settings = {
@@ -846,13 +846,13 @@ static struct msm_gpiomux_config msm_sensor_configs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
 		},
 	},
-	{
-		.gpio = 25, /* WEBCAM2_RESET_N */
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &cam_settings[3],
-			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
-		},
-	},
+	//{
+	//	.gpio = 25, /* WEBCAM2_RESET_N */
+	//	.settings = {
+	//		[GPIOMUX_ACTIVE]    = &cam_settings[3],
+	//		[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
+	//	},
+	//},
 	{
 		.gpio = 26, /* CAM_IRQ */
 		.settings = {
@@ -867,13 +867,13 @@ static struct msm_gpiomux_config msm_sensor_configs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
 		},
 	},
-	{
-		.gpio = 28, /* WEBCAM1_STANDBY */
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &cam_settings[3],
-			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
-		},
-	},
+	//{
+	//	.gpio = 28, /* WEBCAM1_STANDBY */
+	//	.settings = {
+	//		[GPIOMUX_ACTIVE]    = &cam_settings[3],
+	//		[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
+	//	},
+	//},
 	{
 		.gpio = 89, /* CAM1_STANDBY_N */
 		.settings = {
