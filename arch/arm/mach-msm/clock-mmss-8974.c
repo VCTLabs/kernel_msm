1bf660b088347576f38297670090ff85c18ce72b
Stephen Arnold <stephen.arnold42@gmail.com>
2017-05-03 12:54:03 -0700

    mach-msm/clock-mmss-8974.c: ported changes to CLK_LOOKUP_OF
    Signed-off-by: Stephen Arnold <stephen.arnold42@gmail.com>

--- arch/arm/mach-msm/clock-mmss-8974.c
+++ arch/arm/mach-msm/clock-mmss-8974.c
@@ -2302,6 +2302,10 @@ static struct mux_clk mmss_debug_mux = {
 static struct clk_lookup msm_camera_clocks_8974pro_only[] = {
 	CLK_LOOKUP_OF("cam_src_clk", mclk1_clk_src, "90.qcom,camera"),
 	CLK_LOOKUP_OF("cam_clk", camss_mclk1_clk, "90.qcom,camera"),
+	CLK_LOOKUP_OF("cam_src_clk", mclk2_clk_src.c, "20.qcom,camera"),
+	CLK_LOOKUP_OF("cam_src_clk", mclk0_clk_src.c, "20.qcom,eeprom"),
+	CLK_LOOKUP_OF("cam_clk", camss_mclk2_clk.c, "20.qcom,camera"),
+	CLK_LOOKUP_OF("cam_clk", camss_mclk0_clk.c, "20.qcom,eeprom"),
 	CLK_LOOKUP_OF("cam_src_clk", mclk0_clk_src, "0.qcom,camera"),
 	CLK_LOOKUP_OF("cam_src_clk", mclk1_clk_src, "1.qcom,camera"),
 	CLK_LOOKUP_OF("cam_src_clk", mclk2_clk_src, "2.qcom,camera"),
@@ -2313,6 +2317,8 @@ static struct clk_lookup msm_camera_clocks_8974pro_only[] = {
 static struct clk_lookup msm_camera_clocks_8974_only[] = {
 	CLK_LOOKUP_OF("cam_src_clk", mmss_gp1_clk_src, "90.qcom,camera"),
 	CLK_LOOKUP_OF("cam_clk", camss_gp1_clk, "90.qcom,camera"),
+	CLK_LOOKUP_OF("cam_src_clk", mclk0_clk_src.c, "20.qcom,eeprom"),
+	CLK_LOOKUP_OF("cam_clk", camss_mclk0_clk.c, "20.qcom,eeprom"),
 	CLK_LOOKUP_OF("cam_src_clk", mmss_gp0_clk_src, "0.qcom,camera"),
 	CLK_LOOKUP_OF("cam_src_clk", mmss_gp1_clk_src, "1.qcom,camera"),
 	CLK_LOOKUP_OF("cam_clk", camss_gp0_clk, "0.qcom,camera"),
