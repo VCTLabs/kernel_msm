0c91528ae1d42fba4d1603878f87e295ddc046bb
Stephen Arnold <stephen.arnold42@gmail.com>
2017-05-04 23:38:28 -0700

    Revert "USB: Notify OTG about unsupported device and hnp enable failure"
    This needs a proper fix but should do for now, and messages already get
    sent to user space.
    
    This reverts commit ce54dc2ef9137a820cf76bca7ea499d56262e219.

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1732,7 +1732,6 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 #ifdef	CONFIG_USB_OTG_BLACKLIST_HUB
 	if (hdev->parent) {
 		dev_warn(&intf->dev, "ignoring external hub\n");
-		otg_send_event(OTG_EVENT_HUB_NOT_SUPPORTED);
 		return -ENODEV;
 	}
 #endif
@@ -2270,8 +2269,6 @@ out:
 	}
 
 	if (!is_targeted(udev)) {
-
-		otg_send_event(OTG_EVENT_DEV_NOT_SUPPORTED);
 
 		/* Maybe it can talk to us, though we can't talk to it.
 		 * (Includes HNP test device.)
