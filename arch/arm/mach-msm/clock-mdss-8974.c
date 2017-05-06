5bb4c45e736a4e3ad95080519e192c784b856462
Stephen Arnold <stephen.arnold42@gmail.com>
2017-05-06 01:43:42 -0700

    clock-m*-8974.c: OF_CLOCK fixes, possible issues with pll integration
    Signed-off-by: Stephen Arnold <stephen.arnold42@gmail.com>

--- arch/arm/mach-msm/clock-mdss-8974.c
+++ arch/arm/mach-msm/clock-mdss-8974.c
@@ -24,6 +24,8 @@
 #include <asm/processor.h>
 #include <mach/msm_iomap.h>
 
+#include <dt-bindings/clock/msm-clocks-8974.h>
+
 #include "clock-mdss-8974.h"
 
 #define REG_R(addr)			readl_relaxed(addr)
